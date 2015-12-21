7fe4d62074afbc7e592eaaa144ed342fbefdcfed
Alexsandro Oliveira <sandrooliveira1501@gmail.com>
2015-12-21 20:28:01 -0300

    Admob adicionado

--- app/src/main/java/br/ufc/lockscreenwidget/MainActivity.java
+++ app/src/main/java/br/ufc/lockscreenwidget/MainActivity.java
@@ -15,6 +15,9 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_ENABLE = 1001;
@@ -28,6 +31,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest request = new AdRequest.Builder().build();
+        /*AdRequest request = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)        // All emulators
+                .addTestDevice("3AFDADEADF6804E079C53AAAE73A81EC")  // An example device ID
+                .build();*/
+        mAdView.loadAd(request);
+
+
 
         /*LockScreenReceiver receiver = new LockScreenReceiver();
         registerReceiver(receiver, new IntentFilter(Intent.ACTION_SCREEN_ON));
@@ -68,6 +81,8 @@ public class MainActivity extends AppCompatActivity {
         }else{
             ((TextView)findViewById(R.id.tvPassword)).setText("");
         }
+
+
 
     }
 
